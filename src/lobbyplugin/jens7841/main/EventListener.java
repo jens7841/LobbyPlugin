69ea29ea46a6ce559b30ea6ab5231676ef74727f
jens7841 <jens7841@web.de>
2015-10-17 16:20:08 +0200

    Deleted other fun shit xD

--- src/lobbyplugin/jens7841/main/EventListener.java
+++ src/lobbyplugin/jens7841/main/EventListener.java
@@ -1,24 +1,12 @@
 package lobbyplugin.jens7841.main;
 
-import org.bukkit.Bukkit;
-import org.bukkit.GameMode;
-import org.bukkit.Material;
-import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
-import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerRespawnEvent;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
-
-import net.minecraft.server.v1_8_R3.PacketPlayInClientCommand;
-import net.minecraft.server.v1_8_R3.PacketPlayInClientCommand.EnumClientCommand;
 
 public class EventListener implements Listener {
 
@@ -61,65 +49,4 @@ public class EventListener implements Listener {
 			}
 		}
 	}
-
-	@EventHandler
-	public void onPlayerDeath(final PlayerDeathEvent e) {
-		Player p = e.getEntity();
-		Player killer = p.getKiller();
-
-		if (!p.getGameMode().equals(GameMode.SURVIVAL))
-			return;
-		if (killer != null) {
-			p.sendMessage("Du wurdest von " + killer.getName() + " getötet! Er/Sie hatte noch "
-					+ (Math.round(100.0 * (killer.getHealth() / 2)) / 100.0) + " Herzen.");
-			killer.setHealth(20);
-			killer.setFoodLevel(20);
-		}
-
-		Bukkit.getScheduler().scheduleSyncDelayedTask(LobbyPlugin.instance, new Runnable() {
-
-			@Override
-			public void run() {
-				((CraftPlayer) e.getEntity()).getHandle().playerConnection
-						.a(new PacketPlayInClientCommand(EnumClientCommand.PERFORM_RESPAWN));
-			}
-		}, 5);
-
-	}
-
-	@EventHandler
-	public void onPlayerRespawn(final PlayerRespawnEvent e) {
-		Bukkit.getScheduler().scheduleSyncDelayedTask(LobbyPlugin.instance, new Runnable() {
-			@Override
-			public void run() {
-				Player p = e.getPlayer();
-				if (!p.getGameMode().equals(GameMode.SURVIVAL))
-					return;
-
-				for (Player pe : Bukkit.getOnlinePlayers()) {
-					if (!pe.getGameMode().equals(GameMode.SURVIVAL) || pe.getHealth() == 0)
-						continue;
-					Inventory inv = pe.getInventory();
-					inv.clear();
-					inv.setItem(0, new ItemStack(Material.STONE_SWORD));
-					inv.setItem(1, new ItemStack(Material.BOW));
-					inv.setItem(6, new ItemStack(Material.ARROW, 20));
-					inv.setItem(8, new ItemStack(Material.COOKED_BEEF, 5));
-
-					pe.getInventory().setHelmet(new ItemStack(Material.CHAINMAIL_HELMET));
-					pe.getInventory().setChestplate(new ItemStack(Material.CHAINMAIL_CHESTPLATE));
-					pe.getInventory().setLeggings(new ItemStack(Material.CHAINMAIL_LEGGINGS));
-					pe.getInventory().setBoots(new ItemStack(Material.CHAINMAIL_BOOTS));
-					pe.setHealth(20);
-					pe.setFoodLevel(20);
-				}
-			}
-		}, 20);
-	}
-
-	@EventHandler
-	public void onPlayerInteractAtEntityEvent(PlayerInteractAtEntityEvent e) {
-		e.getRightClicked().setPassenger(e.getPlayer());
-	}
-
 }
