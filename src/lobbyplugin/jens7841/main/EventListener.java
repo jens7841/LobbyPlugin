40887465ba660ed9ea3545c3b5b76d1bd22c00cb
jens7841 <jens7841@web.de>
2015-10-18 17:08:13 +0200

    added some events and permissions

--- src/lobbyplugin/jens7841/main/EventListener.java
+++ src/lobbyplugin/jens7841/main/EventListener.java
@@ -4,10 +4,16 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerPickupItemEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class EventListener implements Listener {
@@ -32,7 +38,6 @@ public class EventListener implements Listener {
 				&& (p.getInventory().getHeldItemSlot() == HideItem.getSlot() && e.getItem() != null)
 				&& (e.getItem().equals(HideItem.getHideItem()) || e.getItem().equals(HideItem.getShowItem()))) {
 			HideItem.playerKlick(p, e);
-
 		}
 
 	}
@@ -51,6 +56,9 @@ public class EventListener implements Listener {
 					}
 					e.setCancelled(true);
 				}
+				if (!p.hasPermission(Permissions.EVENT_CAN_MOVE_ITEMS_IN_INVENTORY)) {
+					e.setCancelled(true);
+				}
 				if (!p.hasPermission(Permissions.TELEPORTER_MOVE_IN_INVENTORY)) {
 					if (((slot == TeleporterItem.getSlot() && currentItem.equals(TeleporterItem.getItem())))) {
 						e.setCancelled(true);
@@ -62,15 +70,72 @@ public class EventListener implements Listener {
 						e.setCancelled(true);
 					}
 				}
-				if (!p.hasPermission(Permissions.EVENT_CAN_MOVE_ITEMS_IN_INVENTORY)) {
+			}
+		}
+	}
+
+	@EventHandler
+	public void onDropItem(final PlayerDropItemEvent e) {
+
+		Player p = e.getPlayer();
+
+		ItemStack itemStack = e.getItemDrop().getItemStack();
+		if (!p.hasPermission(Permissions.TELEPORTER_CAN_DROP) && itemStack.equals(TeleporterItem.getItem())) {
+			e.setCancelled(true);
+		}
+		if (!p.hasPermission(Permissions.HIDE_PLAYERS_TOOL_CAN_DROP)
+				&& (itemStack.equals(HideItem.getHideItem()) || itemStack.equals(HideItem.getShowItem()))) {
+			e.setCancelled(true);
+		}
+		if (!p.hasPermission(Permissions.EVENT_CAN_DROP_ITEMS)) {
+			e.setCancelled(true);
+		}
+	}
+
+	@EventHandler
+	public void onPickupItem(PlayerPickupItemEvent e) {
+		Player p = e.getPlayer();
+		if (!p.hasPermission(Permissions.EVENT_CAN_PICKUP_ITEMS)) {
+			e.setCancelled(true);
+		}
+	}
+
+	@EventHandler
+	public void onPlayerRespawn(PlayerRespawnEvent e) {
+		Player p = e.getPlayer();
+		TeleporterItem.giveItem(p);
+		HideItem.giveItem(p);
+	}
+
+	@EventHandler
+	public void onPlayerDeath(PlayerDeathEvent e) {
+		Player p = e.getEntity();
+		if (!p.hasPermission(Permissions.EVENT_DROP_ITEMS_ON_DEATH)) {
+			e.getDrops().clear();
+		}
+	}
+
+	@EventHandler
+	public void onPlayerDamage(EntityDamageEvent e) {
+		if (e.getEntity() instanceof Player) {
+			Player p = (Player) e.getEntity();
+			if (!p.hasPermission(Permissions.EVENT_CAN_GET_DAMAGE)) {
+				DamageCause cause = e.getCause();
+				if (cause != DamageCause.VOID && cause != DamageCause.SUICIDE) {
 					e.setCancelled(true);
 				}
 			}
 		}
 	}
 
-	public void onDropItem(PlayerDropItemEvent e) {
-		Player p = e.getPlayer();
-
+	@EventHandler
+	public void onPlayerHunger(FoodLevelChangeEvent e) {
+		if (e.getEntity() instanceof Player) {
+			Player p = (Player) e.getEntity();
+			if (!p.hasPermission(Permissions.EVENT_CAN_GET_HUNGER)) {
+				e.setCancelled(true);
+			}
+		}
 	}
+
 }
--- src/lobbyplugin/jens7841/main/Permissions.java
+++ src/lobbyplugin/jens7841/main/Permissions.java
@@ -13,6 +13,7 @@ public class Permissions {
 	public static final String TELEPORTER_PREFIX = PREFIX + "teleporter.";
 	public static final String TELEPORTER_USE = TELEPORTER_PREFIX + "use";
 	public static final String TELEPORTER_MOVE_IN_INVENTORY = TELEPORTER_PREFIX + "canmoveitemininventory";
+	public static final String TELEPORTER_CAN_DROP = TELEPORTER_PREFIX + "";
 
 	/*
 	 * Teleporter Commands
@@ -29,6 +30,7 @@ public class Permissions {
 	public static final String HIDE_PLAYERS_TOOL_USE = HIDE_PLAYERS_TOOL_PREFIX + "use";
 	public static final String HIDE_PLAYERS_TOOL_MOVE_ITEM_IN_INVENTORY = HIDE_PLAYERS_TOOL_PREFIX
 			+ "canmoveitemsininventory";
+	public static final String HIDE_PLAYERS_TOOL_CAN_DROP = HIDE_PLAYERS_TOOL_PREFIX + "candrop";
 
 	/*
 	 * Other Commands
@@ -40,5 +42,10 @@ public class Permissions {
 	 * Events
 	 */
 	public static final String EVENT_CAN_MOVE_ITEMS_IN_INVENTORY = PREFIX + "canmoveitemsininventory";
+	public static final String EVENT_CAN_DROP_ITEMS = PREFIX + "candropitems";
+	public static final String EVENT_DROP_ITEMS_ON_DEATH = PREFIX + "dropitemsondeath";
+	public static final String EVENT_CAN_PICKUP_ITEMS = PREFIX + "canpickupitems";
+	public static final String EVENT_CAN_GET_DAMAGE = PREFIX + "cangetdamage";
+	public static final String EVENT_CAN_GET_HUNGER = PREFIX + "cangethunger";
 
 }
